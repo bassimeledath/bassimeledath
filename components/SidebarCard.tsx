7c19be0db87ba3ae69547ae765a2be2df174080c
bassimeledath <bassimfaizal@gmail.com>
2024-09-30 23:50:57 -0700

    refactor: Update Sidebar and SidebarCard components to use next/link for navigation

--- components/SidebarCard.tsx
+++ components/SidebarCard.tsx
@@ -5,7 +5,7 @@ interface SidebarCardProps {
     title: string;
     createdAt: string;
     isSelected: boolean;
-    onClick: (id: string) => void;
+    onClick: () => void;
 }
 
 const SidebarCard: React.FC<SidebarCardProps> = ({ id, title, createdAt, isSelected, onClick }) => {
@@ -17,7 +17,7 @@ const SidebarCard: React.FC<SidebarCardProps> = ({ id, title, createdAt, isSelec
     return (
         <div
             className={`p-5 border-b border-[#413530] cursor-pointer rounded-lg ${isSelected ? 'bg-[#9e7c29]' : ''}`}
-            onClick={() => onClick(id)}
+            onClick={onClick}
         >
             <h3 className="text-base font-semibold text-gray-200 mb-1">{title}</h3>
             <p className="text-sm text-gray-400">{formatDate(createdAt)}</p>
